d451062f9fd3eefc68e9a2b3524141af3a232a82
Simon Fraser <sfraser@mrpfd.com>
2018-07-20 09:10:28 +0100

    installed react

---
+++ src/Splash.js
@@ -0,0 +1,20 @@
+import React  from 'react';
+import './Splash.css';
+
+class Splash extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return <div className="pageWrapper">
+            Content
+        </div>
+    }
+}
+
+export default Splash
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Splash from './Splash';
+
+ReactDOM.render(<Splash />, document.getElementById('root'));
