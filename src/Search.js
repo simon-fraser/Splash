ab3343f9fc4b59aad7db4996fbd2bb95f07189c5
Simon Fraser <sfraser@mrpfd.com>
2018-07-28 13:50:58 +0100

    wip updating layout and styles.

--- src/Search.js
+++ src/Search.js
@@ -2,14 +2,12 @@ import React from 'react';
 import './Search.css';
 
 class Search extends React.Component {
-    
+
     render() {
         return <div className="search">
-            <form method="GET" action="https://www.google.co.uk/search">
+            <form method="GET" action="https://duckduckgo.com/">
                 <input className="search-input" type="search" name="q" placeholder="Search now..." ref="searchnow" autoFocus />
-                <button type="submit" className="search-button">
-                    <img src={`${process.env.PUBLIC_URL}/search.svg`} alt="search" />
-                </button>
+                <button type="submit" className="search-button"></button>
             </form>
         </div>
     }
--- src/Splash.js
+++ src/Splash.js
@@ -1,8 +1,7 @@
 import React  from 'react';
-import axios from 'axios';
+import base64Img from 'base64-img-promise';
 import Search from './Search';
 import Weather from './Weather';
-import base64Img from 'base64-img-promise';
 import { OPEN_WEATHER_KEY } from './env';
 import './Splash.css';
 
@@ -34,15 +33,21 @@ class Splash extends React.Component {
         let resolution = [];
         resolution[0] = screenWidth;
         resolution[1] = (screenWidth / 1.6);
+
         // High Quality Multiplier
         if(window.devicePixelRatio > 1) {
             resolution[0] = (resolution[0]*2)
             resolution[1] = (resolution[1]*2)
         }
         this.setState({ backgroundSize: resolution.join('x') })
-        
+
+        // console.log((now - setuptime))
+        // console.log((hours*60*60*1000))
+        // console.log((localstate !== null && setuptime !== null))
+        // console.log(((now - setuptime) < (hours*60*60*1000)))
+
         // If storage load state from previous storage!
-        if((localstate && setuptime) && ((now - setuptime) < (hours*60*60*1000))) {
+        if((localstate !== null && setuptime !== null) && ((now - setuptime) < (hours*60*60*1000))) {
             this.setState(JSON.parse(localstate));
         } else {
             // Run all the commands to generate splash page
@@ -56,7 +61,6 @@ class Splash extends React.Component {
                 localStorage.setItem('splash$setup', now)
             });
         }
-        
     }
 
     // Get coordinates from browser
@@ -65,6 +69,7 @@ class Splash extends React.Component {
             enableHighAccuracy: true,
             maximumAge: 0
         }
+
         return new Promise(function (resolve, reject) {
             navigator.geolocation.getCurrentPosition(resolve, reject, options);
         });
@@ -72,20 +77,9 @@ class Splash extends React.Component {
 
     // Query UnSplash for a nice background, return as a base64 image string
     get64Background() {
-        return base64Img.requestBase64('https://source.unsplash.com/random/' + this.state.backgroundSize)
+        return base64Img.requestBase64(`https://source.unsplash.com/random/${this.state.backgroundSize}`)
         .then(function({res, data}) {
             return data
-        })
-        .catch(function (error) { return {'error': error} });
-    }
-
-    // Query UnSplash for a nice background
-    getBackground() {
-
-        return axios.get('https://source.unsplash.com/random/' + this.state.backgroundSize)
-        .then(function (response) {
-            if(response.status === 200) return response.request.responseURL;
-            return {'error': 'no response'};
         })
         .catch(function (error) { return {'error': error} });
     }
@@ -94,22 +88,11 @@ class Splash extends React.Component {
     getWeather() {
         return this.getCoordinates().then(function(resolve) {
             if(resolve.coords) {
-                return axios.get(
-                    'https://api.openweathermap.org/data/2.5/weather',
-                    {
-                        timeout: 1000,
-                        params: {
-                            lat: resolve.coords.latitude,
-                            lon: resolve.coords.longitude,
-                            APPID: OPEN_WEATHER_KEY,
-                            units: 'metric'
-                        }
-                    }
-                )
-                .then(function (response) {
-                    if(response.status === 200) return response.data;
-                    return {'error': 'no response'};
+                return fetch(`https://api.openweathermap.org/data/2.5/weather?APPID=${OPEN_WEATHER_KEY}&lat=${resolve.coords.latitude}&lon=${resolve.coords.longitude}&units=metric`)
+                .then(blob => {
+                    if(blob.status === 200) return blob.json()
                 })
+                .then(data => {return data})
                 .catch(function (error) { return {'error': error} });
             }
         });
@@ -117,18 +100,17 @@ class Splash extends React.Component {
 
     // Combine and display our splash
     render() {
+
         return <React.Fragment>
             { (this.state.loaded) ?
-                <div className="lander" style={{ backgroundImage: `url(${this.state.background})` }}>
-                    
+                <div className="lander" style={{ backgroundImage:`url(${this.state.background})` }}>
                     <div className="container">
                         <Search />
                         <Weather weather={this.state.weather} />
                     </div>
-
                 </div>
             :
-                <div className="loading"><img src={`${process.env.PUBLIC_URL}/rolling.svg`} alt="Loading" /></div> 
+                <div className="loading"><img src={`${process.env.PUBLIC_URL}/rolling.svg`} alt="Loading" /></div>
             }
         </React.Fragment>
     }
